e08dc5701a71feb15c3ef0d104cddac509e2d7b3
MaTtiaa01 <mattia2001@live.it>
2022-12-07 16:07:45 +0100

    printi in page some attributes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ class Movie
     public $title;
     public $description;
     public $link;
-    public $sconto = 0;
+    public $discount = 0;
     public $genre;
 
     public function __construct(string $title, string $description, string $link, array $genre)
@@ -20,15 +20,15 @@ class Movie
     public function setDiscount($age)
     {
         if ($age > 45) {
-            $this->sconto = 20;
+            $this->discount = 20;
         } else if ($age < 18) {
-            $this->sconto = 50;
+            $this->discount = 50;
         }
     }
 
     public function getDiscount()
     {
-        return $this->sconto;
+        return $this->discount;
     }
 }
 
@@ -41,15 +41,94 @@ $Genres = [
 
 $avatar = new Movie("Avatar", "lorem ipsum", "www.avatar.com", $Genres);
 $avatar->setDiscount(16);
-$scontoAvatar = $avatar->getDiscount();
-var_dump($scontoAvatar);
+$discountAvatar = $avatar->getDiscount();
+var_dump($discountAvatar);
 
 $matrix = new Movie("Matrix", "lorem ipsum 2", "ww.matrix.com", $Genres);
 $matrix->setDiscount(70);
-$scontomatrix = $matrix->getDiscount();
-var_dump($scontomatrix);
+$discountmatrix = $matrix->getDiscount();
+var_dump($discountmatrix);
 
 var_dump($avatar->title);
 var_dump($matrix->title);
 
 var_dump($avatar->genre);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row">
+            <div class="col">
+                <h2>Title</h2>
+                <ul>
+                    <li>
+                        <?php echo $avatar->title; ?>
+                    </li>
+                    <li>
+                        <?php echo $matrix->title; ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>Description</h2>
+                <ul>
+                    <li>
+                        <?php echo $avatar->description; ?>
+                    </li>
+                    <li>
+                        <?php echo $matrix->description; ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>Link</h2>
+                <ul>
+                    <li>
+                        <?php echo $avatar->link; ?>
+                    </li>
+                    <li>
+                        <?php echo $matrix->link; ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>discount</h2>
+                <ul>
+                    <li>
+                        <?php echo $avatar->discount; ?>
+                    </li>
+                    <li>
+                        <?php echo $matrix->discount; ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="col">
+                <h2>Genre</h2>
+                <ul>
+                    <li>
+
+                    </li>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
