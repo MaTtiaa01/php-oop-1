37b0322175ac86ad972824d049c33d8e82ccc1d2
MaTtiaa01 <mattia2001@live.it>
2022-12-07 15:50:27 +0100

    add bonus Genre.php

--- Models/Genre.php
+++ Models/Genre.php
@@ -1,1 +1,9 @@
 <?php
+class Genre
+{
+    public $genre;
+    public function __construct(string $genre)
+    {
+        $this->genre = $genre;
+    }
+}
--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 
+require __DIR__ . '/Models/Genre.php';
 class Movie
 {
     public $title;
     public $description;
     public $link;
     public $sconto = 0;
+    public $genre;
 
-    public function __construct(string $title, string $description, string $link)
+    public function __construct(string $title, string $description, string $link, array $genre)
     {
         $this->title = $title;
         $this->description = $description;
         $this->link = $link;
+        $this->genre = $genre;
     }
 
     public function setDiscount($age)
@@ -30,15 +33,23 @@ class Movie
 }
 
 
-$avatar = new Movie("Avatar", "lorem ipsum", "www.avatar.com");
+$Genres = [
+    new Genre("Fantasy"),
+    new Genre("Horror")
+];
+
+
+$avatar = new Movie("Avatar", "lorem ipsum", "www.avatar.com", $Genres);
 $avatar->setDiscount(16);
 $scontoAvatar = $avatar->getDiscount();
 var_dump($scontoAvatar);
 
-$matrix = new Movie("Matrix", "lorem ipsum 2", "ww.matrix.com");
+$matrix = new Movie("Matrix", "lorem ipsum 2", "ww.matrix.com", $Genres);
 $matrix->setDiscount(70);
 $scontomatrix = $matrix->getDiscount();
 var_dump($scontomatrix);
 
 var_dump($avatar->title);
 var_dump($matrix->title);
+
+var_dump($avatar->genre);
